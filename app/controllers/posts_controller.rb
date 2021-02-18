25da8bfb98b2635e61fd0c84b83f7887d926bbc9
Yehuda Bortz <yehudabortz@icloud.com>
2021-02-18 15:18:44 -0600

    commit tree 17a0a4598a838aaa9a16ca9d07aef2a384aca927

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,18 @@
 class PostsController < ApplicationController
+  def new
+  end
+
+  def create
+    @post = Post.new([post_params])
+    if @post.valid?
+      @post.save
+      redirect_to post_path(@post)
+    else
+      render :new
+    end
+  end
+
+
   def show
     @post = Post.find(params[:id])
   end
@@ -6,13 +20,20 @@ class PostsController < ApplicationController
   def edit
     @post = Post.find(params[:id])
   end
-
+  
   def update
+    # binding.pry
     @post = Post.find(params[:id])
 
-    @post.update(post_params)
+    if @post.update(post_params)
 
-    redirect_to post_path(@post)
+
+      redirect_to post_path(@post)
+    else
+      render :edit
+    end
+
+
   end
 
   private
